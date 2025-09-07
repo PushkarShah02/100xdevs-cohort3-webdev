d7d35335111b643eaf774f0c141d85eb482f41de
Pushkar Shah <piyushah02003@gmail.com>
2025-09-07 12:11:28 +0530

    Bad way to do dark/ light mode

--- Week-13-Tailwind_class/13.2/src/App.jsx
+++ Week-13-Tailwind_class/13.2/src/App.jsx
@@ -1,9 +1,15 @@
 import SidebarCard from "./components/basic-project-1";
+import { useState } from "react";
 
 function App() {
+  const [darkmode, setDarkmode]= useState(true)
+
+  function flipMode(){
+    setDarkmode(c=>!c)
+  }
   return (
-    <div>
-      <SidebarCard />
+    <div className={`h-screen ${darkmode ? "bg-blue-900" : "bg-white"}`}>
+      <button className="bg-grey-500" onClick={flipMode}>Click Me</button>
     </div>
   );
 }
